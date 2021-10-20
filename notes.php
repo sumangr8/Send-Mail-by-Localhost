5f224e270930e3fb2af601f47ca0fa80235bf789
sumangr8 <30518152+sumangr8@users.noreply.github.com>
2021-10-20 21:27:09 +0530

    Create notes.php

---
+++ notes.php
@@ -0,0 +1,35 @@
+xamp->sendmail->sendmail.ini  config file
+smtp_server=smtp.gmail.com
+smtp_port=587 ya 465
+smtp_ssl=auto
+error_logfile=error.log
+auth_username=suman.krgr8@gmail.com
+auth_password=cmmmjqdegkilgkpv
+
+now go ti php.ini file
+SMTP= smtp.gmail.com
+smtp_port=587 ya 465
+sendmail_from = suman.krgr8@gmail.com
+sendmail_path ="\"D:\xamp\sendmail\sendmail.exe\" -t"
+
+
+gmail login
+go to manage your google account
+security
+click on 2-step varification
+click get started
+enter your current password
+click continue
+enter your number and send
+enter otp and next
+now turn on
+
+now you goto again  
+go to manage your google account
+security
+and click app password
+enter your current password
+click select app and choose other
+enter any remark
+copy password
+jtvixslrioggolzy   ye password sendmail.ini file pe use karna hai
