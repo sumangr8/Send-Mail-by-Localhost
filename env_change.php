aacdedf903c663f10322142de8e0bf039a21b473
sumangr8 <30518152+sumangr8@users.noreply.github.com>
2022-04-22 10:25:11 +0530

    Create env_change.php

---
+++ env_change.php
@@ -0,0 +1,8 @@
+MAIL_MAILER=smtp
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=465
+MAIL_USERNAME=pmbocerp@gmail.com
+MAIL_PASSWORD=svjqycklagioxgcq
+MAIL_ENCRYPTION=ssl
+MAIL_FROM_ADDRESS=pmbocerp@gmail.com
+MAIL_FROM_NAME="${APP_NAME}"
